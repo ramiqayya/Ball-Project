e7636553a776827baf2baf568e08184713fe29ae
ramiqayya <rami.qayya@gmail.com>
2021-10-20 01:12:14 +0300

    PickUps
    PickUps added and UI

--- New Ball Project/Assets/Scripts/PlayerController.cs
+++ New Ball Project/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    private int count;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     private void OnMove(InputValue movementValue)
@@ -22,10 +30,29 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
         
     }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
+
+    }
 
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement*speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
     }
 }
